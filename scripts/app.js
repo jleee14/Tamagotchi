eed400c602a47fef1b91818f206b3ba6c228d275
jleee14 <kjleee14@gmail.com>
2022-02-08 15:59:12 -0800

    added sleep on/off functionality to button

--- scripts/app.js
+++ scripts/app.js
@@ -7,6 +7,7 @@ let boredom = 10;
 let hunger = 10;
 let sleepiness = 10;
 let isSleeping = false;
+let sleepInterval;
 
 // Starter functions
 const startAge = function() {
@@ -17,18 +18,38 @@ const startAge = function() {
 const startHunger = function(){
     hunger -= .01;
     $("#hunger__bar").text(hunger);
+    if (hunger <= 0) {
+        $("#hunger__bar").text(0);
+    }
 
 }
 
 const startSleepiness = function() {
     sleepiness -= .01;
     $("#sleepiness__bar").text(sleepiness);
+    if (sleepiness <= 0) {
+        $("#sleepiness__bar").text(0);
+    }
 }
 
 const startBoredom = function() {
     boredom -= .01;
     $("#boredom__bar").text(boredom);
+    if (boredom <= 0) {
+        $("#boredom__bar").text(0);
+    }
 }
+
+const sleepRegen = function() {
+    sleepiness += .05;
+    $("#sleepiness__bar").text(sleepiness);
+}
+
+
+const stopSleep = function(){
+    clearInterval(sleepInterval);
+}
+
 
 const startGame = function () {
     setInterval(startAge, 2000);
@@ -60,4 +81,14 @@ $("#button__play").on("click", function(){
     } else {
         boredom += 0;
     }
+});
+
+$("#button__sleep").on("click", function(){
+    isSleeping = !isSleeping;
+    if (isSleeping) {
+        sleepInterval = setInterval(sleepRegen, 50);
+    }
+    else if (!isSleeping) {
+        stopSleep();
+    } 
 });
